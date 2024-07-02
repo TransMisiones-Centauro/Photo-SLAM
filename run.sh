f32fbf39dceefc24c89600c2e07e14bf56da0d0c
Enric Cervera <ecervera@uji.es>
2024-07-02 15:56:06 +0200

    mount scripts folder

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ docker run --gpus all -it --privileged --ipc=host --pid=host \
   -e NVIDIA_DRIVER_CAPABILITIES=all -e DISPLAY \
   -v /dev:/dev -v /tmp/.X11-unix/:/tmp/.X11-unix \
   -v $PWD/data:/data -v $PWD/results:/results \
+  -v $PWD/scripts:/Photo-SLAM/scripts \
   photo_slam:latest
 
 xhost -si:localuser:root
